2bd97007db66bd1aaa8fc27680a2eaa726c763be
Kevin Mark <kmark937@gmail.com>
2015-01-09 05:52:37 -0500

    Add missing copyright header

--- app/src/main/java/com/versobit/kmark/xhangouts/MmsApnConfigDialog.java
+++ app/src/main/java/com/versobit/kmark/xhangouts/MmsApnConfigDialog.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014-2015 Kevin Mark
+ *
+ * This file is part of XHangouts.
+ *
+ * XHangouts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XHangouts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XHangouts.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.versobit.kmark.xhangouts;
 
 import android.app.AlertDialog;
